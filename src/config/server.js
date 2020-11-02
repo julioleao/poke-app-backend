39df0f28959b5c9c1e85164a70ca458808054d26
julioleao <julio0023@live.com>
2020-11-02 11:17:02 -0300

    Fix environment port connection

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,5 @@
 const badyParser = require('body-parser');
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const server = express();
@@ -9,7 +10,7 @@ server.use(badyParser.urlencoded({ extended: true }));
 server.use(badyParser.json());
 server.use(cors());
 
-server.listen(port, () => {
+server.listen(process.env.PORT || port, () => {
   console.log(`Backend is running on port ${port}.`);
 });
 
